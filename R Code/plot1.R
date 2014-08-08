6c4022c48478b566dd961de63d0762db5989e38d
TimothyJRides <timcarroll@gmail.com>
2014-08-08 15:45:46 -0400

    Code and plots added.

---
+++ R Code/plot1.R
@@ -0,0 +1,29 @@
+# Table downloaded from 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# on 8/6/2014
+
+# Read table from unzipped file
+
+# setwd("Coursera/04 - ExpDataAnalysis/ProjAssign1")
+powerdata <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";"
+                        , stringsAsFactors = FALSE)
+
+# Subset data to 2007-02-01 and 2007-02-02
+
+powerdata$Date <- strptime(paste(powerdata$Date,powerdata$Time), "%d/%m/%Y %H:%M:%S")
+powerdata <- subset(powerdata, Date >= "2007-02-01" 
+                    & Date <"2007-02-03")
+
+# Make data numeric
+
+powerdata[,3:9] <- as.numeric(unlist(powerdata[,3:9]))
+
+# Open png device, run histogram and close device.
+
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px")
+
+hist(powerdata$Global_active_power, xlab = "Global Active Power (kilowatts)"
+     , main = "Global Active Power", col = "red")
+
+dev.off()
---
+++ R Code/plot3.R
@@ -0,0 +1,32 @@
+# Table downloaded from 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# on 8/6/2014
+
+# Read table from unzipped file
+
+# setwd("Coursera/04 - ExpDataAnalysis/ProjAssign1")
+powerdata <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";"
+                        , stringsAsFactors = FALSE)
+
+# Subset data to 2007-02-01 and 2007-02-02
+
+powerdata$Date <- strptime(paste(powerdata$Date,powerdata$Time), "%d/%m/%Y %H:%M:%S")
+powerdata <- subset(powerdata, Date >= "2007-02-01" 
+                    & Date <"2007-02-03")
+
+# Make data numeric
+
+powerdata[,3:9] <- as.numeric(unlist(powerdata[,3:9]))
+
+# Open png device, run histogram and close device.
+
+png(filename = "plot3.png",
+    width = 480, height = 480, units = "px")
+
+plot(powerdata$Date, powerdata$Sub_metering_1, type = "l"
+     ,ylab = "Energy sub metering",xlab = "")
+lines(powerdata$Date, powerdata$Sub_metering_2, col = "red")
+lines(powerdata$Date, powerdata$Sub_metering_3, col = "blue")
+legend("topright", c(colnames(powerdata[7:9])),lwd = c(2,2), col = c("black","red","blue"))
+
+dev.off()
